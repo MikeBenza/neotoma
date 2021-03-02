797099789b56dfcdb9f5b729ba29808cd0ef1b65
Mike Benza <mike.benza@alertlogic.com>
2021-03-01 23:02:34 -0600

    Copy test from https://github.com/seancribbs/neotoma/pull/32/commits/3ecb836f4dfe69b0433b834389c215b8ff25d4ae

--- test/test_parse.erl
+++ test/test_parse.erl
@@ -32,3 +32,18 @@ case_insensitive_test() ->
     catch
         _:_  -> ?assert(false)
     end.
+
+unicode_parser_test() ->
+    Data = "rule <- [\\x{1}-\\x{D7FF}]+;",
+    file:write_file("test_unicode_parser.peg", io_lib:fwrite("~s\n", [Data])),
+    neotoma:file("test_unicode_parser.peg"),
+    compile:file("test_unicode_parser.erl", []),
+    try
+        TestString =  [19990,30028,32,102,111,111],
+        Result = test_unicode_parser:parse(TestString),
+        ?assertEqual(6, length(Result)),
+        StringResult = lists:flatten(io_lib:format("~ts", [Result])),
+        ?assertEqual(TestString, StringResult)
+    catch
+        _:_  -> ?assert(false)
+    end.
